36349f7fded1513ac7a1d4b791779335ec407563
Rossiitaa <rosario.scuotto@outlook.it>
2022-06-23 18:53:35 +0200

    bonus finished

--- js/main.js
+++ js/main.js
@@ -2,26 +2,67 @@
 
     const dinamicGrid = document.getElementById('grid-dinamic');
     
-    const extractedElements = [];
-    
+    const selectionButton = document.getElementById('diff-button');
+
 
     buttonPlay.addEventListener('click', function(){
         dinamicGrid.innerHTML = null;
+        let val = document.getElementById("diff-button").value;
+    if(val === "0") {
+        for (let i = 0; i < 100; i++) {
 
-    for (let i = 0; i < 100; i++) {
-        const newBox = createNewBox();
-        newBox.innerHTML = i
-        newBox.addEventListener('click' , function(){
-            newBox.classList.toggle('azzurro');
-            console.log(newBox.innerHTML);
-        })
-        dinamicGrid.append(newBox);
+            const newBox = createNewBox();
+            newBox.classList.add('rs_box_consegna');
+
+            newBox.addEventListener('click' , function(){
+
+                newBox.classList.toggle('azzurro');
+                console.log(newBox.innerHTML);
+            });
+
+            newBox.innerHTML = i;
+            dinamicGrid.append(newBox);
+        }
     }
-})
 
+    if(val === "1") {
+        for (let i = 0; i < 81; i++) {
 
-    function createNewBox() {
-        const currentBox = document.createElement('div');
-        currentBox.classList.add('rs_box_consegna');
-        return currentBox;
+            const newBox = createNewBox();
+            newBox.classList.add('rs_box_consegna_int');
+
+            newBox.addEventListener('click' , function(){
+
+                newBox.classList.toggle('azzurro');
+                console.log(newBox.innerHTML);
+            });
+
+            newBox.innerHTML = i;
+            dinamicGrid.append(newBox);
+        }
     }
+
+    if(val === "2") {
+        for (let i = 0; i < 49; i++) {
+
+            const newBox = createNewBox();
+            newBox.classList.add('rs_box_consegna_diff');
+
+            newBox.addEventListener('click' , function(){
+
+                newBox.classList.toggle('azzurro');
+                console.log(newBox.innerHTML);
+            });
+
+            newBox.innerHTML = i;
+            dinamicGrid.append(newBox);
+        }
+    }
+
+});
+
+function createNewBox() {
+    const currentBox = document.createElement('div');
+    currentBox.classList.add('square');
+    return currentBox;
+}
